a59049724fe11272b1a014d6cfd9fb5504b2c837
Fritsch <fritsc13@ads.uni-passau.de>
2019-12-20 20:17:00 +0100

    Push pdynmc on GitHub

---
+++ R/package-pdynmc.R
@@ -0,0 +1,40 @@
+#' pdynmc: A package for moment conditions based estimation of linear dynamic
+#'    panel data models
+#'
+#' The pdynmc package provides three categories of functions that are available
+#'    to the user: A function for model fitting, functions for specification
+#'    testing, and functions that extract and summarize particular information
+#'    from fitted model objects.
+#'
+#' @docType package
+#' @name pdynmc
+NULL
+
+## NULL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
